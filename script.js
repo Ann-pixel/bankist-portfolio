ca84720fa1219d47466e83bbb435eb30522ae633
Gauri Mhaiskar <gauri.mhaiskar@gmail.com>
2021-02-01 10:16:25 -0500

    got a basic understanding of styles & added initial event listeners and query selectors

--- script.js
+++ script.js
@@ -2,3 +2,27 @@
 
 ///////////////////////////////////////
 // Modal window
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".btn--close-modal");
+const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
+
+function openModal() {
+  modal.classList.remove(".hidden");
+  overlay.classList.remove(".hidden");
+}
+function closeModal() {
+  modal.classList.add(".hidden");
+  overlay.classList.add(".hidden");
+}
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener("click", openModal);
+}
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (evt) {
+  if (evt.key === "Escape" && !modal.classList(".hidden")) {
+    closeModal();
+  }
+});
